c6028a399476c5b41b988a715565115a70ecad69
CastenWang <1198664769@qq.com>
2024-06-30 18:26:16 +0800

    temp

--- http/http_conn.cpp
+++ http/http_conn.cpp
@@ -20,5 +20,17 @@ void http_conn::initmysql_result(connection_pool *connPool)
 {
     // 从连接池中取一个连接
     MYSQL *mysql = NULL;
-    connectionRAII mysqlcon(&mysql,connPool);
+    connectionRAII mysqlcon(&mysql, connPool);
+
+    // 在user表中检索username passwd数据 浏览器端输入
+    if (mysql_query(mysql, "SELECT username,passwd FROM user"))
+    {
+        LOG_ERROR("SELECT error;%s\n", mysql_error(mysql));
+    }
+
+    // 从表中检索完整的结果集
+    MYSQL_RES *result = mysql_store_result(mysql);
+
+    // 返回结果集中的列数
+    int num_fields = mysql_num_fields(result);
 }
